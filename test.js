54f6e0d0dd3b37fb827f19a06053c2c5fd739037
Ivan-Marysh <166243361+Ivan-Marysh@users.noreply.github.com>
2025-08-23 18:46:07 +0300

    add 44

--- test.js
+++ test.js
@@ -3,29 +3,39 @@ console.log("1. Створіть об'єкт 'bankAccount' з властивос
 
 
 
-const bankAccount = {
-    ownerName: "no name",
-    accountNumber: "33333",
-    balance: 10000,
+// const bankAccount = {
+//   ownerName: "Jack Blue",
+//   accountNumber: "33333",
+//   balance: 10000,
 
-deposit(sum){
-    if(sum > 0){
-        this.balance += sum;
-        alert(`поповнення завершено, балнс ${this.balance} гривень`);
-    } else {
-        alert("поповнення немажливо на 0 чи менше");
-    }
-},
+// deposit(){
+//   const sum = Number(prompt("Введіть суму для поповнення"));
+//   if(isNaN(sum) || sum <= 0){   
+//       alert("поповнення немажливо на 0 чи менше");
+//   } else {
+//     this.balance += sum;
+//     alert(`поповнення завершено, баланс ${this.balance} гривень`);
+//   }
+// },
 
 
-withdraw(sum){
-  if(sum <= 0){
-    alert("списання на 0 немажливо");
-  } else if (sum > this.balance) {
-    alert("недастаньо коштів");
-  } else {
-    this.balance -= sum;
-    alert(`списання завершеноб баданс ${this.balance} гривень`);
-  }
-},
-};
+// withdraw(){
+//   const sum = Number(prompt("Введіть суму для отримання коштів"));
+// if(isNaN(sum) || sum <= 0){
+//   alert("списання на 0 немажливо");
+// } else if (sum > this.balance) {
+//   alert("недастанточно коштів");
+// } else {
+//   this.balance -= sum;
+//   alert(`списання завершено баланс ${this.balance} гривень`);
+// }
+// },
+// };
+
+// bankAccount.deposit();
+// bankAccount.withdraw();
+
+
+console.log("2. Створіть об'єкт weather з властивостями temperature, humidity, windSpeed. Додайте до об'єкту метод, який повертає true, якщо температура нижче 0 градусів Цельсія, та false, якщо температура вище або рівна 0 градусів Цельсія. Температуру потрібно отримати з prompt(). Якщо метод повернув true вивести повідомлення “температура нижче 0 градусів Цельсія” і навпаки");
+
+
